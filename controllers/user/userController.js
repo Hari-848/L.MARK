a51ab22609cac535fd7738b8dc1f97035e41385f
harichandran8 <minichandranmini08175@gmail.com>
2025-05-30 14:35:32 +0530

    otp verify

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -267,6 +267,7 @@ exports.verifyOTP = async (req, res) => {
     
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
+    console.log('Error in verifyOTP:', error);
     res.status(500).json({ message: 'Server error' });
   }
 };
